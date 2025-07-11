fe9c8fc3c5a391976f6a52180d76467b74587720
IceyPanda23 <ronaldkipngetich80@gmail.com>
2025-07-11 09:51:35 +0300

    commented joshua to edit query

--- update.py
+++ update.py
@@ -109,19 +109,6 @@ def update_div(localhost=None):
             for div_no in div_list:
                 div_no = int(div_no)
                 try:
-                    # try:
-                    #     div_no_float = float(div_no)
-                    #     div_no_int = int(div_no_float)
-
-                    #     # Check if it was a whole number
-                    #     if div_no_float != div_no_int:
-                    #         raise ValueError("Not a whole number")
-
-                    #     div_no = div_no_int
-                    # except (ValueError, TypeError):
-                    #     print(f"⚠️ Skipping invalid DividendNo '{div_no}' (non-integer or malformed)")
-                    #     failed_list.append(div_no)
-                    #     continue
                     if not str(div_no).isdigit():
                         print(f"⚠️ Skipping invalid DividendNo '{div_no}' (non-numeric)")
                         failed_list.append(div_no)
@@ -141,17 +128,6 @@ def update_div(localhost=None):
                                 FROM {table_name} INNER JOIN
                                 Dividend ON {table_name}.sno = Dividend.Shareholderno AND {table_name}.divno = Dividend.DividendNo
                             """
-#                         update_query_mpesa = f"""
-#                             UPDATE Dividend
-#                             SET DividendPaymentDate = [{table_name}].[date], DividendPaymentMethodCode = 25
-#                             FROM Dividend INNER JOIN
-#                             [{table_name}] ON Dividend.Shareholderno = [{table_name}].[sno] AND Dividend.DividendNo = [{table_name}].[divno]"""
-#                         update_query_mpesa_2 =  f"""
-#                             UPDATE [{table_name}]
-#                             SET matched = 1
-#                             FROM Dividend INNER JOIN
-#                             [{table_name}] ON Dividend.Shareholderno = [{table_name}].[sno] AND Dividend.DividendNo = [{table_name}].[divno]
-# """
                         else:
                             update_query = f"""
                                 UPDATE Dividend
@@ -168,6 +144,20 @@ def update_div(localhost=None):
                             """
 #                         update_que
                     else:
+# JOSHUA EDIT HEREEEEE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# JOSHUA EDIT HEREEEEE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# JOSHUA EDIT HEREEEEE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# JOSHUA EDIT HEREEEEE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# JOSHUA EDIT HEREEEEE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# JOSHUA EDIT HEREEEEE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# JOSHUA EDIT HEREEEEE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# JOSHUA EDIT HEREEEEE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# JOSHUA EDIT HEREEEEE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# JOSHUA EDIT HEREEEEE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# JOSHUA EDIT HEREEEEE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# JOSHUA EDIT HEREEEEE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# JOSHUA EDIT HEREEEEE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
                         if db_name != "EABLDatabaseRegister":
                             update_query = f"""
                                                 UPDATE Dividend{div_no}
@@ -196,33 +186,6 @@ def update_div(localhost=None):
                                 FROM {table_name} INNER JOIN
                                 Dividend{div_no} ON {table_name}.sno = Dividend{div_no}.Shareholderno AND {table_name}.divno = Dividend{div_no}.DividendNo
                             """
-                        # else:
-                        #     update_query = f"""
-                        #         UPDATE Dividend{div_no}
-                        #         SET DividendPaymentDate = {table_name}.Date, DividendPaymentMethodCode = {Payment_code}, DividendPaid = 1
-                        #         FROM Dividend{div_no} INNER JOIN
-                        #         {table_name} ON Dividend{div_no}.ShareholderNo = {table_name}.sno AND Dividend{div_no}.DividendNo = {table_name}.divno
-                        #     """
-
-                        #     update_query_2 = f"""
-                        #         UPDATE {table_name}
-                        #         SET matched = 1
-                        #         FROM {table_name} INNER JOIN
-                        #         Dividend{div_no} ON {table_name}.sno = Dividend{div_no}.Shareholderno AND {table_name}.divno = Dividend{div_no}.DividendNo
-                        #     """
-#                         update_query_mpesa = f"""
-#                             UPDATE Dividend{div_no}
-#                             SET DividendPaymentDate = [{table_name}].[date], DividendPaymentMethodCode = 25
-#                             FROM Dividend{div_no} INNER JOIN
-#                             [{table_name}] ON Dividend{div_no}.Shareholderno = [{table_name}].[sno] AND Dividend{div_no}.DividendNo = [{table_name}].[divno]"""
-#                         update_query_mpesa_2 =  f"""
-#                             UPDATE [{table_name}]
-#                             SET matched = 1
-#                             FROM Dividend{div_no} INNER JOIN
-#                             [{table_name}] ON Dividend{div_no}.Shareholderno = [{table_name}].[sno] AND Dividend{div_no}.DividendNo = [{table_name}].[divno]
-# """
-                    # Payment_update = input("Input Payment Update")
-                    # paymentmethod = input("Please input your payment Method:")
                     result = connection.execute(text(update_query))
                     print(f"✅ Dividend{div_no}: {result.rowcount} rows updated")
                     result2 = connection.execute(text(update_query_2))
